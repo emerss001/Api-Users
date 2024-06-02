ff918f6a0c59936453072a939fd7b75032219072
emerss001 <emersonnevess80@gmail.com>
2024-06-02 20:23:42 -0300

    feat: configuring route to register new users

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,28 @@
-console.log("object");
+import { PrismaClient } from "@prisma/client"
+import express, { Request, Response } from "express"
+
+const prisma = new PrismaClient()
+const app = express()
+app.use(express.json())
+
+interface User {
+    name: string
+    email: string
+    age: number
+}
+
+app.post("/users", async (req: Request, res: Response) => {
+    const newUser: User = req.body
+
+    await prisma.user.create({
+        data: {
+            name: newUser.name,
+            email: newUser.email,
+            age: newUser.age,
+        },
+    })
+
+    res.status(201).json(newUser)
+})
+
+app.listen(3000)
